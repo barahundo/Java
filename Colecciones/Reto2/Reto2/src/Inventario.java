425546141a6d5fd1df76f376dbad756d57818298
Barahundo <juanplag@hotmail.com>
2021-08-09 15:26:01 -0500

    V1.1

--- Colecciones/Reto2/Reto2/src/Inventario.java
+++ Colecciones/Reto2/Reto2/src/Inventario.java
@@ -1,11 +1,36 @@
-import java.util.Arrays;
 import java.util.Scanner;
+import Cafe.Arabigo;
+import Cafe.Robusta;
 public class Inventario {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
         System.out.println();
         String ln=sc.nextLine();
         String []rv=ln.split("&");
-        System.out.println(Arrays.asList(rv));
+        String opc= rv[0];
+           while (opc!="3"){        
+            if (opc=="1"){
+                int id= Integer.parseInt(rv[2]);
+                float precio=Float.parseFloat(rv[3]);
+                boolean molido=Boolean.parseBoolean(rv[4]);
+                /*String n=Integer.toString(count);
+                String variable="obj"+n;*/
+                if (rv[1]=="Arabigo"){
+                    Arabigo ob=new Arabigo(id, precio, molido, rv[5], rv[6]);
+                    ob.impObj();
+                }else if (rv[1]=="Robusta"){
+                    float cant=Float.parseFloat(rv[5]);
+                    Robusta ob=new Robusta(id, precio, molido, cant, rv[1]);
+                    ob.impObj();
+                }
+            }else if (opc=="2"){
+                //ob.impObj();
+            }
+            System.out.println();
+            ln=sc.nextLine();
+            rv=ln.split("&");         
+        }
+        sc.close();
+        System.exit(0);
     }
 }
